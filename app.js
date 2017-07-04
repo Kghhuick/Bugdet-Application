cbc1a36e6708d537bd09f6cf749e96d232082427
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-04 12:11:44 +0200

    share application on independents modules

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+var bugdetController = (function(){
+
+console.log("kamil");
 
 
+})();
+
+
+
+var UIcontroller = (function(){
+
+
+
+
+
+})();
+
+
+
+
+var controller = (function(bugdetCtrl,UIctrl){
+
+
+
+
+
+
+})(bugdetController,UIcontroller);
 
 
 
