41472a9e362552d54466d4d343ef15be010ea6c7
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-04 16:22:06 +0200

    add initialization function

--- app.js
+++ app.js
@@ -4,52 +4,62 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   var UIcontroller = (function() {
 
-  var DOMstrings = {
-   inputType : ".add__type",
-   inputDescription :  ".add__description",
-   inputValue : ".add__value",
-   inputBtn : ".add__btn"
- };
-
-    return {
-      getInput : function(){
-        return {
-         type : document.querySelector(DOMstrings.inputType).value,
-         description : document.querySelector(DOMstrings.inputDescription).value,
-         value : document.querySelector(DOMstrings.inputValue).value,
-      }
-    }
-
-    getDOMstrings : function(){
-      return DOMstrings;
-
-    }
-
+    var DOMstrings = {
+      inputType: ".add__type",
+      inputDescription: ".add__description",
+      inputValue: ".add__value",
+      inputBtn: ".add__btn"
     };
 
+    return {
+      getInput: function() {
+        return {
+          type: document.querySelector(DOMstrings.inputType).value,
+          description: document.querySelector(DOMstrings.inputDescription).value,
+          value: document.querySelector(DOMstrings.inputValue).value
+        }
+      },
+
+      getDOMstrings: function() {
+        return DOMstrings;
+
+      }
+
+    };
 
   })();
 
   var controller = (function(bugdetCtrl, UIctrl) {
+    var setUpEventListners = function () {
+      var DOM = UIctrl.getDOMstrings();
+      document.querySelector(DOM.inputBtn).addEventListener("click", ctr);
 
-     var DOM = UIctrl.getDOMstrings();
+      document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
 
-    var ctr = function() { 
+          ctr();
+        }
+
+      });
+    };
+
+    var ctr = function() {
       var input = UIctrl.getInput();
       console.log(input);
 
     }
 
-    document.querySelector(".DOM.inputBtn").addEventListener("click", ctr)
+    return {
+     init : function () {
+      console.log("working");
+      setUpEventListners() ;
+    }
+ }
 
-    document.addEventListener("keypress", function(event) {
-      if (event.keyCode === 13 || event.which === 13) {
-
-        ctr();
-      }
-
-    });
 
   })(bugdetController, UIcontroller);
+
+
+  controller.init();
 
 });
