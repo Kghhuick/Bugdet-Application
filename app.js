1b83aeb8dba99630d4061db01f067aa02440d1f9
Kamil Ciaston <Kamilciaston6@gmail.com>
2017-07-21 22:33:39 +0200

    some DOM manipulations

--- app.js
+++ app.js
@@ -96,7 +96,11 @@ var UIcontroller = (function() {
     inputValue: ".add__value",
     inputBtn: ".add__btn",
     incomeContainer: ".income__list",
-    expensesContainer: ".expenses__list"
+    expensesContainer: ".expenses__list",
+    budgetLabel:".budget__value",
+    incomeLabel:".budget__income--value",
+    expensesLabel:".budget__expenses--value",
+    percentageLabel:".budget__expenses--percentage"
   };
 
   return {
@@ -154,6 +158,20 @@ var UIcontroller = (function() {
 
     },
 
+   displayBudget : function(obj) {
+    document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+     document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc
+       document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+
+         if(obj.percentage > 0) {
+      document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%";
+    }else{
+
+       document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+    }
+   },
+
     getDOMstrings: function() {
       return DOMstrings;
 
@@ -183,7 +201,7 @@ var controller = (function(budgetCtrl, UIctrl) {
 
      var budget = budgetCtrl.getBudget();
 
-     console.log(budget);
+     UIctrl.displayBudget(budget);
 
 
   }
@@ -209,6 +227,13 @@ var controller = (function(budgetCtrl, UIctrl) {
   return {
     init: function() {
       console.log("working");
+      UIctrl.displayBudget({
+        budget:0,
+        totalInc:0,
+        totalExp:0,
+        percentage:-1
+
+      });
       setUpEventListners();
     }
   }
